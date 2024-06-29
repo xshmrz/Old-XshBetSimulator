ef2677712f186403ffe70f54d1c44743d2b2cb69
xshmrz <xshmrz@gmail.com>
2024-06-29 16:11:56 +0300

    Update

--- app/Console/Commands/AppDoItEnum.php
+++
@@ -1,8 +0,0 @@
-<?php
-    namespace App\Console\Commands;
-    final class AppDoItEnum {
-        public static $MODULES                = ["site", "panel", "dashboard", "api"];
-        public static $ELEMENTS               = ["GetBtn", "GetFrm", "GetMdl", "GetMdlBtn", "GetAllBtn", "GetAllFrm", "GetAllMdl", "GetAllMdlBtn", "CreateBtn", "CreateFrm", "CreateMdl", "CreateMdlBtn", "UpdateBtn", "UpdateFrm", "UpdateMdl", "UpdateMdlBtn", "DeleteBtn", "DeleteFrm", "DeleteMdl", "DeleteMdlBtn"];
-        public static $AUTHORIZATIONS         = ["login", "register", "lost-password", "logout"];
-        public static $AUTHORIZATIONSELEMENTS = ["Btn", "Frm", "Mdl", "MdlBtn"];
-    }
