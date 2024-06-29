a1ebeb8f7a61bb89068b60b6b31d1c52b2ab8d29
xshmrz <xshmrz@gmail.com>
2024-06-30 02:06:43 +0300

    Update

--- resources/views/Site/Desktop/Home/index.blade.php
+++ resources/views/Site/Desktop/Home/index.blade.php
@@ -1,5 +1,15 @@
 @extends("Site.Desktop.layout")
 @section("section-main")
+
+    <?php
+        if (!requestHas(status)):
+            $status = EnumProjectStatus::Pending;
+        else:
+            $status = request()->get(status);
+        endif;
+        $couponData = Coupon()->where([status => $status])->orderBy(id, "DESC")->get();
+    ?>
+
 	<main id="main-container">
 		<div class="bg-body-light border-bottom">
 			<div class="content py-2 text-end">
@@ -21,30 +31,42 @@
 			</div>
 		</div>
 		<div class="content content-full">
-			<div class="row">
-				@foreach(Coupon()->orderBy(id,"DESC")->get() as $coupon)
-					<div class="col-md-12">
-						<div class="block block-rounded shadow">
-							<div class="block-header block-header-default p-3 bg-body-dark">
-								<h3 class="block-title">{{$coupon->no}}</h3>
-								<div>
-									<span class="badge rounded bg-{{EnumProjectStatus::getColor($coupon->status)}} text-uppercase text-end min-width-100px">{{number_format($coupon->odd * 1000),2}} TL</span>
-									<span class="badge rounded bg-primary text-uppercase min-width-50px">{{number_format($coupon->odd,2)}}</span>
+			<ul class="nav nav-pills space-x-1 push">
+				<li class="nav-item"><a class="nav-link {{EnumProjectStatus::Pending == $status ? "active" : null}}" href="{{request()->fullUrlWithQuery([status => EnumProjectStatus::Pending])}}">Pending</a></li>
+				<li class="nav-item"><a class="nav-link {{EnumProjectStatus::Win == $status ? "active" : null}}" href="{{request()->fullUrlWithQuery([status => EnumProjectStatus::Win])}}">Win</a></li>
+				<li class="nav-item"><a class="nav-link {{EnumProjectStatus::Lost == $status ? "active" : null}}" href="{{request()->fullUrlWithQuery([status => EnumProjectStatus::Lost])}}">Lost</a></li>
+			</ul>
+			@if($couponData->isEmpty())
+				<div class="alert alert-secondary" role="alert">
+                    <p class="mb-0">
+                      There Is No {{EnumProjectStatus::getTranslation($status)}} Coupon
+                    </p>
+				</div>
+			@else
+				<div class="row">
+					@foreach($couponData as $coupon)
+						<div class="col-md-12">
+							<div class="block block-rounded shadow">
+								<div class="block-header block-header-default p-3 bg-body-dark">
+									<h3 class="block-title">{{$coupon->no}}</h3>
+									<div>
+										<span class="badge rounded bg-{{EnumProjectStatus::getColor($coupon->status)}} text-uppercase text-end min-width-100px">{{number_format($coupon->odd * 1000),2}} TL</span>
+										<span class="badge rounded bg-primary text-uppercase min-width-50px">{{number_format($coupon->odd,2)}}</span>
+									</div>
 								</div>
-							</div>
-							<div class="block-content p-0 border-top">
-								<ul class="list-group list-group-flush push mb-0">
-									@foreach(Bet()->whereIn(marketNo,explode(",",$coupon->data))->orderBy(eventDate,"ASC")->get() as $bet)
-										<li class="list-group-item d-flex justify-content-between align-items-start p-3">
+								<div class="block-content p-0 border-top">
+									<ul class="list-group list-group-flush push mb-0">
+										@foreach(Bet()->whereIn(marketNo,explode(",",$coupon->data))->orderBy(eventDate,"ASC")->get() as $bet)
+											<li class="list-group-item d-flex justify-content-between align-items-start p-3">
 											<div class="me-auto">
 												<div class="fw-bold">{{Str::limit($bet->eventName,25,"...")}}</div>
 												<div class="text-muted">{{now()::parse($bet->eventDate)->format("d-m-Y H:i")}}</div>
 											</div>
 											@if($bet->live == EnumProjectFinish::Yes)
-												<div class="me-3">
+													<div class="me-3">
 													<i class="fa fa-spinner fa-spin"></i>
 												</div>
-											@endif
+												@endif
 											<div class="me-2">
 												<div><span class="badge rounded bg-body-light text-dark min-width-50px mb-1">{{$bet->score}}</span></div>
 												<div>&nbsp;</div>
@@ -54,17 +76,18 @@
 												<div><span class="badge rounded bg-primary min-width-50px">{{number_format($bet->odd,2)}}</span></div>
 											</div>
 										</li>
-									@endforeach
-								</ul>
-							</div>
-							<div class="block-content d-flex justify-content-between align-items-start  p-3 border-top bg-body-light">
-								<div>&nbsp;</div>
-								<div></div>
+										@endforeach
+									</ul>
+								</div>
+								<div class="block-content d-flex justify-content-between align-items-start  p-3 border-top bg-body-light">
+									<div>&nbsp;</div>
+									<div></div>
+								</div>
 							</div>
 						</div>
-					</div>
-				@endforeach
-			</div>
+					@endforeach
+				</div>
+			@endif
 		</div>
 	</main>
 @endsection
